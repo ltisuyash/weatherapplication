1fa6877a345cfee52a920d5b846f5fd98c40b992
LTIMindtreeDev86 <vasudev.pai@ltimindtree.com>
2024-10-22 17:00:27 +0530

    hard code

--- src/main/java/com/example/weatherservice/service/WeatherApi.java
+++ src/main/java/com/example/weatherservice/service/WeatherApi.java
@@ -1,5 +1,6 @@
 package com.example.weatherservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.example.weatherservice.model.Weather;
@@ -8,8 +9,12 @@ import com.example.weatherservice.model.Weather;
 public class WeatherApi {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String apiUrl = "https://api.openweathermap.org/data/2.5/weather";
-    private final String apiKey = "1122e5a53be72e7ced513e190d590ae1";
+
+    @Value("${weather.api.url}")
+    private String apiUrl;
+
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     public Weather getWeather(String city) {
         String url = String.format("%s?q=%s&appid=%s", apiUrl, city, apiKey);
