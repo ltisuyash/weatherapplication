7f68540250b9056e6680f578e6794ad1ce3d5a1a
LTIMindtreeDev86 <vasudev.pai@ltimindtree.com>
2024-10-23 17:32:06 +0530

    Update LambdaHandler.java

--- src/main/java/com/example/weatherservice/LambdaHandler.java
+++ src/main/java/com/example/weatherservice/LambdaHandler.java
@@ -15,20 +15,19 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 @Component
 public class LambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(WeatherServiceApplication.class);
         } catch (ContainerInitializationException e) {
-            // if we fail here. We re-throw the exception to force another cold start
-            e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
- 
+
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
     }
 }
