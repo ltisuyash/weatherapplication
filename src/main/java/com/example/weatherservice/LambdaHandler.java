45390d69f82544b33f22634e1120ad7b05bb04ea
LTIMindtreeDev86 <vasudev.pai@ltimindtree.com>
2024-10-23 16:35:05 +0530

    lamda change

--- src/main/java/com/example/weatherservice/LambdaHandler.java
+++ src/main/java/com/example/weatherservice/LambdaHandler.java
@@ -23,35 +23,26 @@ public class LambdaHandler implements RequestHandler<Map<String, Object>, Weathe
     @Override
     public Weather handleRequest(Map<String, Object> input, Context context) {
         try {
-            context.getLogger().log("Received input: " + input);
             String source = (String) input.get("source");
-          /*  if ("aws.events".equals(source)) {
+            if ("aws.events".equals(source)) {
                 // Handle EventBridge event
-                context.getLogger().log("EventBridge event received");
-                // Process the event as needed
                 return null; // or return a default Weather object
-            } else {*/
+            } else {
                 // Handle API Gateway event
                 Object headersObj = input.get("headers");
                 if (headersObj instanceof Map) {
                     Map<?, ?> headersMap = (Map<?, ?>) headersObj;
-                    context.getLogger().log("Headers map: " + headersMap);
-                    headersMap.forEach((key, value) -> context.getLogger().log("Header: " + key + " = " + value));
                     String city = (String) headersMap.get("city");
                     if (city == null || city.isEmpty()) {
                         throw new IllegalArgumentException("City cannot be null or empty");
                     }
                     return weatherService.fetchAndSaveWeather(city);
-					/*
-					 * } else { context.getLogger().log("Invalid headers format: " + headersObj);
-					 * throw new IllegalArgumentException("Invalid headers format"); }
-					 */
+                } else {
+                    throw new IllegalArgumentException("Invalid headers format");
+                }
             }
         } catch (Exception e) {
-            // Log the error and return a meaningful response or rethrow the exception
-            context.getLogger().log("Error processing request: " + e.getMessage());
             throw e;
         }
-        return null; 
     }
 }
